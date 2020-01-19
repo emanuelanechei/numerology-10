365f43628dac13c2a6f556b44e4642c537b83e10
Takeshi Mizumoto <t.mizumoto@hylable.com>
2020-01-19 10:26:43 +0900

    Refactoring

--- numerology.py
+++ numerology.py
@@ -44,33 +44,159 @@ def filter_consonant(string):
     return "".join([s for s in string.lower() if s not in vowels])
 
 
-def count_words(string):
-    values = alpha2value(string)
-    ret = {}
-    for i in range(1, 10):
-        occurrs = len([tmp for tmp in values if tmp == i])
-        if occurrs == 0:
-            label = "欠落数"
-        elif occurrs >= 3:
-            label = "特性数"
+class Numerology:
+    def __init__(self, year, month, day, name):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.name = name.replace(" ", "").lower()
+
+        self.is_calculated = False
+
+    def __str__(self):
+        if self.is_calculated:
+            out = "あなたの入力\n"
+            out += "    誕生年 %d\n" % self.year
+            out += "    誕生月 %d\n" % self.month
+            out += "    誕生日 %d\n" % self.day
+            out += "    名前　 %s\n" % self.name
+            out += "\n"
+            out += "誕生数\t %s\n" % self.tanjo
+            out += "運命数\t %s\n" % self.unmei
+            out += "実現数\t %s\n" % self.jitsugen
+            out += "ハート数 %s\n" % self.heart
+            out += "人格数\t %s\n" % self.jinkaku
+            out += "習慣数\t %s\n" % self.shukan
+
+            out += "特性数/欠落数\n"
+            for number in self.tokusei:
+                occurs, label = self.tokusei[number]
+                out += ("    %d [%d回] %s\n" % (number, occurs, label))
+
+            out += "気質のバランス\n"
+            for key in self.balance:
+                out += "    %s\t%d\n" % (key, self.balance[key])
+
+            out += "時間区分\n"
+            for i, term in enumerate(self.terms):
+                out += "    第%d期 %s〜%s\t%d\t%d\n" % (
+                    i+1, term["区分"][0], term["区分"][1], term["頂点数"], term["試練数"]
+                )
+            out += "個人年リスト\n"
+            for year, pyear in self.pyears:
+                out += "    %d --> 個人年%d\n" % (year, pyear)
         else:
-            label = ""
-        ret[i] = (occurrs, label)
-    return ret
+            out = "VALUE NOT AVAILABLE"
+        return out
 
+    def calc_chart(self):
+        self.tanjo = self.get_tanjo()
+        self.unmei = self.get_unmei()
+        self.jitsugen = self.get_jitsugen()
+        self.heart = self.get_heart()
+        self.jinkaku = self.get_jinkaku()
+        self.shukan = self.get_shukan()
+        self.tokusei = self.get_tokusei()
+        self.balance = self.get_balance()
+        self.terms = self.get_terms()
+        self.pyears = self.get_personal_years()
 
-def count_balance(string):
-    values = alpha2value(string)
-    conds = {
-        "身体数": [4, 5],
-        "知性数": [1, 8],
-        "感情数": [2, 3, 6],
-        "直感数": [7, 9]
-    }
-    ret = {}
-    for key in conds:
-        ret[key] = len([v for v in values if v in conds[key]])
-    return ret
+        self.is_calculated = True
+
+    def get_tanjo(self):
+        """誕生数の計算"""
+        return shrink(shrink(self.year) + shrink(self.month) + shrink(self.day))
+
+    def get_unmei(self):
+        """運命数の計算"""
+        return shrink(sum(alpha2value(self.name)))
+
+    def get_jitsugen(self):
+        """実現数の計算"""
+        return shrink(self.tanjo + self.unmei)
+
+    def get_heart(self):
+        """ハート数の計算"""
+        return shrink(sum(alpha2value(filter_vowel(self.name))))
+
+    def get_jinkaku(self):
+        """人格数の計算"""
+        return shrink(sum(alpha2value(filter_consonant(self.name))))
+
+    def get_shukan(self):
+        """習慣数の計算"""
+        return shrink(len(self.name))
+
+    def get_tokusei(self):
+        """特性数/欠落数の計算"""
+        values = alpha2value(self.name)
+        ret = {}
+        for i in range(1, 10):
+            occurrs = len([tmp for tmp in values if tmp == i])
+            if occurrs == 0:
+                label = "欠落数"
+            elif occurrs >= 3:
+                label = "特性数"
+            else:
+                label = ""
+            ret[i] = (occurrs, label)
+        return ret
+
+    def get_balance(self):
+        values = alpha2value(self.name)
+        conds = {
+            "身体数": [4, 5],
+            "知性数": [1, 8],
+            "感情数": [2, 3, 6],
+            "直感数": [7, 9]
+        }
+        ret = {}
+        for key in conds:
+            ret[key] = len([v for v in values if v in conds[key]])
+        return ret
+
+    def get_terms(self):
+        """年齢区分と、区分ごとの頂点数と試練数を計算"""
+        # 事前の計算
+        syear = shrink(self.year)
+        smonth = shrink(self.month)
+        sday = shrink(self.day)
+
+        # 年齢区分
+        terms = []
+        terms.append(("誕生", 36-self.tanjo))
+        terms.append((terms[-1][1]+1, terms[-1][1]+9))
+        terms.append((terms[-1][1]+1, terms[-1][1]+9))
+        terms.append((terms[-1][1]+1, "終生"))
+
+        # 頂点数
+        choten = [
+            shrink(smonth + sday, False),
+            shrink(sday + syear, False),
+            shrink((smonth + sday) + (sday + syear), False),
+            shrink(smonth + syear, False),
+        ]
+
+        # 試練数
+        shiren = [
+            shrink(abs(smonth - sday), False),
+            shrink(abs(sday - syear), False),
+            shrink(abs(abs(smonth - sday) - abs(sday - syear)), False),
+            shrink(abs(smonth - syear), False),
+        ]
+
+        # 組み合わせる
+        merged = ({
+            "区分": terms[i], "頂点数": choten[i], "試練数": shiren[i]
+        } for i in range(4))
+        return merged
+
+    def get_personal_years(self, begin=2019, end=2030):
+        pyears = []
+        for y in range(begin, end):
+            pyear = shrink(shrink(y) + shrink(day) + shrink(month))
+            pyears.append((y, pyear))
+        return pyears
 
 
 if __name__ == "__main__":
@@ -86,84 +212,8 @@ if __name__ == "__main__":
     month = args.month
     day = args.day
     name = args.name.replace(" ", "").lower()
-    print("""
-あなたの入力
-    誕生年   %d
-    誕生月   %d
-    誕生日   %d
-    名前    %s
-    """ % (year, month, day, name))
 
+    obj = Numerology(year, month, day, name)
+    obj.calc_chart()
+    print(obj)
 
-    # 誕生数
-    tan = shrink(shrink(year) + shrink(month) + shrink(day))
-    print("誕生数\t", tan)
-
-    # 運命数
-    unn = shrink(sum(alpha2value(name)))
-    print("運命数\t", unn)
-
-    # 実現数
-    print("実現数\t", shrink(tan + unn))
-
-    # ハート数
-    vow = filter_vowel(name)
-    print("ハート数", shrink(sum(alpha2value(filter_vowel(name)))))
-
-    # 人格数
-    print("人格数\t", shrink(sum(alpha2value(filter_consonant(name)))))
-
-    # 習慣数
-    print("習慣数\t", shrink(len(name)))
-
-    # 特性数/欠落数
-    print("特性数/欠落数")
-    count = count_words(name)
-    for number in count:
-        occurs, label = count[number]
-        print("    %d [%d回] %s" % (number, occurs, label))
-
-    # 気質のバランス
-    count = count_balance(name)
-    for key in count:
-        print(key, count[key])
-
-    # 年齢区分
-    terms = []
-    terms.append(("誕生", 36-tan))
-    terms.append((terms[-1][1]+1, terms[-1][1]+9))
-    terms.append((terms[-1][1]+1, terms[-1][1]+9))
-    terms.append((terms[-1][1]+1, "終生"))
-
-    # 頂点数
-    syear = shrink(year)
-    smonth = shrink(month)
-    sday = shrink(day)
-
-    cho = [
-        shrink(smonth + sday, False),
-        shrink(sday + syear, False),
-        shrink((smonth + sday) + (sday + syear), False),
-        shrink(smonth + syear, False),
-    ]
-
-    # 試練数
-    shi = [
-        shrink(abs(smonth - sday), False),
-        shrink(abs(sday - syear), False),
-        shrink(abs(abs(smonth - sday) - abs(sday - syear)), False),
-        shrink(abs(smonth - syear), False),
-    ]
-
-    print("\n年齢区分")
-    for i in range(4):
-        print("    第%d期 %s〜%s\t%d\t%d" % (
-            i+1, terms[i][0], terms[i][1],
-            cho[i], shi[i]
-        ))
-
-    # 個人年
-    print("個人年リスト")
-    for y in range(2020, 2030):
-        pyear = shrink(shrink(y) + shrink(day) + shrink(month))
-        print("    %d --> 個人年%d" % (y, pyear))
